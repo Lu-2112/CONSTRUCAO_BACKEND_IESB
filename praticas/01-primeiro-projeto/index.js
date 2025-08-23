06b1597e682a97efed74bdb6ecc517a1326c5e9d
Lu-2112 <eduardadfluiza@gmail.com>
2025-08-22 21:34:52 -0300

    Criando primeiro projeto node e projeto calculadora de nota

---
+++ praticas/01-primeiro-projeto/index.js
@@ -0,0 +1,18 @@
+console.log("Meu primeiro projeto!!!")
+
+console.log("Hello")
+
+let nome = 'Luiza'
+
+let sobreNome = "Eduarda"
+
+let idade = 19
+
+console.log("Olá " + nome + " " + sobreNome)
+console.log("Sua idade é " + idade)
+
+if(idade >= 18){
+    console.log("Você é maior de idade!!!")
+}else{
+    console.log("Você é menor de idade!!!")
+}
