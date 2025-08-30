5bc30f363fbac3a5505396df6c4ef4923a060c13
Lu-2112 <eduardadfluiza@gmail.com>
2025-08-29 21:44:29 -0300

    Aula de hoje, testes da calculadora

---
+++ praticas/02-calculadora-nota/CalculadoraNota.test.js
@@ -0,0 +1,16 @@
+// importar um código  que vai ser testado 
+let {calcularNotaA1} = require('./CalculadoraNota')
+
+// importo as funcionalidade do JEST
+let{describe, expect, test} = require('@jest/globals')
+
+// describe para fazer o agrupamento dos testes 
+describe('Testando Modulo CalculadoraNota', () => {
+    // construir os testes unitários
+    test('calcularNotaA1 -> ex 1, trb 3, prov 6 = 10', () => {
+        expect(calcularNotaA1(1,3,6)).toBe(10)
+    })
+    test('calcularNotaA1 -> ex 0, trb 0, prov 0 = 0', () => {
+        expect(calcularNotaA1(0, 0, 0)).toBe(0)
+    })
+})
